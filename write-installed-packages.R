b834d7c7046c6a9fc778a59b75fb98245d561756
Schwartz <joel@joelschwartz.com>
2020-01-27 16:37:14 -0800

    Add write-installed-packages.r

---
+++ write-installed-packages.R
@@ -0,0 +1,26 @@
+## deja vu from earlier!
+
+## create a data frame of your installed packages
+## hint: installed.packages() is the function you need
+library(tidyverse)
+
+d = installed.packages() %>%
+  as_tibble()
+
+## optional: select just some of the variables, such as
+##   * Package
+##   * LibPath
+##   * Version
+##   * Priority
+##   * Built
+d = d %>% select(Package:Priority, Built)
+
+## write this data frame to data/installed-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+write_csv(d, here::here("data", "installed-packages.csv"))
+write_csv(d, "data/installed-packages.csv")
+
+## YES overwrite the file that is there now (or delete it first)
+## that's a old result from me (Jenny)
+## it an example of what yours should look like and where it should go
